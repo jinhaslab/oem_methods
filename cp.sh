0d4f86c0ab155c5ebd7160cca11b84d27d9feeff
jinhaslab <jinhaslab@gmail.com>
2022-08-15 20:21:32 +0900

    second

---
+++ cp.sh
@@ -0,0 +1,6 @@
+cp -rf /home/dspub/project/methods/_book/* /home/dspub/apps/doc/methods/
+
+
+
+
+scp /home/sehnr/bookdown/dspubprj/img/influenza*.png dspub@34.64.97.196:/home/dspub/project/methods/img
